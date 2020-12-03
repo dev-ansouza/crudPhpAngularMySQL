429b7b2fbabe74284e822c239ad5defc1befadb9
Arthur Nunes <arthurnx98@gmail.com>
2020-12-03 18:43:00 -0300

    Iniciada criação do backend para futuras execuções na listagem

---
+++ src/app/usuarios.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  //Define url static
+  URL = "http://localhost/api/";
+
+  constructor(private http: HttpClient) { }
+
+  //get users
+  getUsers() {
+    return this.http.get(`${this.URL}GetUsers.php`);
+  }
+
+
+  //update user
+  updateUser(user) {
+    return this.http.post(`${this.URL}UpdateUser.php`, JSON.stringify(usuario));
+  }
+
+  //search user for idUser
+  searchUser(idUser: number) {
+    return this.http.get(`${this.URL}searchUser.php?idUser=${idUser}`);
+  }
+
+  //select user
+  selectUser(idUser : number) {
+    return this.http.get(`${this.URL}SelectUser.php?idUser=${idUser}`);
+  }
+
+  //edit user
+  editUser(user) {
+    return this.http.post(`${this.URL}EditUser.php`, JSON.stringify(user));
+  }
+
+}
